59a7fcee1906d498251e53e9377a18ba7caf00a0
GitHub Action <action@github.com>
2025-04-09 07:46:13 +0000

    Release 0.8.1

--- sky/__init__.py
+++ sky/__init__.py
@@ -5,7 +5,7 @@ from typing import Optional
 import urllib.request
 
 # Replaced with the current commit when building the wheels.
-_SKYPILOT_COMMIT_SHA = '{{SKYPILOT_COMMIT_SHA}}'
+_SKYPILOT_COMMIT_SHA = '590899536f2a4115ee60a9e71e483705d8fd9b30'
 
 
 def _get_git_commit():
@@ -35,7 +35,7 @@ def _get_git_commit():
 
 
 __commit__ = _get_git_commit()
-__version__ = '1.0.0-dev0'
+__version__ = '0.8.1'
 __root_dir__ = os.path.dirname(os.path.abspath(__file__))
 
 
